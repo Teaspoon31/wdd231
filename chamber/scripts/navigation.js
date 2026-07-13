c3b1b6c958aa0d5b4b11aa2718cdad7ca1e1dde0
teaspoon31 <jboateng4@byupathway.edu>
2026-07-13 16:25:34 +0000

    add

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -2,9 +2,5 @@ const menuButton = document.getElementById('menu');
 const navLinks = document.getElementById('navLinks');
 
 menuButton.addEventListener('click', () => {
-  if (navLinks.style.display === 'flex') {
-    navLinks.style.display = 'none';
-  } else {
-    navLinks.style.display = 'flex';
-  }
+  navLinks.classList.toggle('show');
 });
